c3d35ce793ff68ecd9cf6be9eb85048163924603
FelienZ <rajabfadh@gmail.com>
2025-12-12 00:48:38 +0700

    Define main root App & Routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,34 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import './index.css'
-import App from './App.tsx'
+import {BrowserRouter, Route, Routes} from 'react-router'
+import Store from './components/Layout/Store'
+import Home from './frontend/Home/main'
+import Dashboard from './frontend/Store/main'
+import Help from './frontend/Help/main'
+import ThemeProvider from './utils/util/ThemeProvider'
+import ProductDetail from './frontend/Store/ProductDetail/main';
+import NotFound from './frontend/404';
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+          <ThemeProvider>
+          <Routes>
+            <Route path='/' element={<Store/>}>
+              <Route path='' index element={<Home/>}/>
+              <Route path='products' element={<Dashboard/>}/>
+              <Route path='products/:id' element={<ProductDetail/>}/>
+              <Route path='help' element={<Help/>}/>
+              <Route path='*' element={<NotFound/>}/>
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>,
 )
