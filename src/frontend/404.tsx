a1017078452c6a201de20a81cba60c15def2bde2
FelienZ <rajabfadh@gmail.com>
2025-12-12 00:46:01 +0700

    Define Not Found page

---
+++ src/frontend/404.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button"
+import { useNavigate } from "react-router"
+
+export default function NotFound(){
+    const navigate = useNavigate()
+    return(
+        <article className="flex flex-col justify-center items-center min-h-screen gap-4">
+            <img src="/Kuchistore.svg" alt="storelogo" className="w-[5vw]"/>
+            <div className="flex flex-col items-center gap-2">
+                <h2 className="font-bold text-lg">404</h2>
+                <p className="font-light">The page is Not Found</p>
+            </div>
+            <Button variant={'secondary'} onClick={()=> navigate('/')}>Home</Button>
+        </article>
+    )
+}
