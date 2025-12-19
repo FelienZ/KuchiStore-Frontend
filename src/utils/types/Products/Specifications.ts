dffafb10050f95cd9d431a6b1c4e181789169461
FelienZ <rajabfadh@gmail.com>
2025-12-19 20:17:08 +0700

    Define some types & interfaces for Products data

---
+++ src/utils/types/Products/Specifications.ts
@@ -0,0 +1,14 @@
+export interface ProductSpecifications {
+  os: string;
+  gpu: string;
+  port: string;
+  size: string;
+  brand: string;
+  camera: string;
+  memory: string;
+  display: string;
+  network: string;
+  storage: string;
+  prosesor: string;
+  resolution: string;
+}
