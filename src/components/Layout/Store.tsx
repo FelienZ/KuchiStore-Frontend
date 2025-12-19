f802cca4cc4bf71a23dd29c289f3c0f9e749712b
FelienZ <rajabfadh@gmail.com>
2025-12-19 20:02:02 +0700

    Optimize store layout

--- src/components/Layout/Store.tsx
+++ src/components/Layout/Store.tsx
@@ -3,19 +3,21 @@ import Navbar from "../Common/Navbar";
 import '@/app.css'
 import { SidebarProvider, SidebarTrigger } from "../ui/sidebar";
 import { AppSidebar } from "../Common/Sidebar";
-import ScrollHelper from "@/utils/util/scrollTop";
+import ScrollHelper from "@/utils/util/Helper/scrollTop";
+import { Toaster } from "../ui/sonner";
 export default function Store(){
     ScrollHelper()
     return(
         <SidebarProvider /* defaultOpen */>
             <AppSidebar/>
-            <section className="font-[Poppins] grid min-h-screen text-foreground overflow-x-auto scrollbar-hide">
+            <section className="font-[Poppins] grid text-foreground overflow-x-auto scrollbar-hide">
                 <SidebarTrigger className="fixed top-[30vh] z-30 bg-background border rounded-none rounded-r-md"/>
                 <Navbar/>
-                <section className="w-[95vw] min-h-screen py-5 place-self-center">
+                <Toaster/>
+                <section className="w-screen min-h-screen my-5">
                     <Outlet/>
                 </section>
-        </section>
+            </section>
         </SidebarProvider>
     )
 }
