b32b1a572d934f6589f2a03ebe065ed6dbf9cbab
FelienZ <rajabfadh@gmail.com>
2025-12-26 17:14:15 +0700

    Modify Login & Register Form structure

--- src/components/Common/Form/Login.tsx
+++ src/components/Common/Form/Login.tsx
@@ -1,26 +1,32 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { Spinner } from "../../ui/spinner"
-import type { FormAction } from "@/utils/types/Form"
-import type { Login } from "@/utils/types/Auth/Request/Login"
-import { useNavigate } from "react-router"
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Spinner } from "../../ui/spinner";
+import type { FormAction } from "@/utils/types/Form";
+import type { Login } from "@/utils/types/Auth/Request/Login";
+import { useNavigate } from "react-router";
 
-export function FormLogin({type, HandleSubmit, setData, data, isPending}: FormAction<Login>) {
-  const navigate = useNavigate()
+export function FormLogin({
+  type,
+  HandleSubmit,
+  setData,
+  data,
+  isPending,
+}: FormAction<Login>) {
+  const navigate = useNavigate();
   return (
-    <Card className="w-full max-w-sm rounded-md">
+    <Card className="w-[30%] max-md:w-[80%] rounded-sm">
       <CardHeader>
         <CardTitle className="text-center">{type}</CardTitle>
       </CardHeader>
-        <form onSubmit={(e)=> HandleSubmit(e)}>
+      <form onSubmit={(e) => HandleSubmit(e)}>
         <CardContent>
           <div className="flex flex-col gap-5">
             <div className="grid gap-2">
@@ -28,7 +34,7 @@ export function FormLogin({type, HandleSubmit, setData, data, isPending}: FormAc
               <Input
                 id="email"
                 type="email"
-                onChange={(e)=> setData({...data, email: e.target.value})}
+                onChange={(e) => setData({ ...data, email: e.target.value })}
                 placeholder="email@example.com"
                 required
               />
@@ -37,12 +43,12 @@ export function FormLogin({type, HandleSubmit, setData, data, isPending}: FormAc
               <div className="flex items-center">
                 <Label htmlFor="password">Password</Label>
               </div>
-              <Input 
-                id="password" 
+              <Input
+                id="password"
                 type="password"
-                onChange={(e)=> setData({...data, password: e.target.value})}
-                placeholder="Masukkan Password" 
-                required 
+                onChange={(e) => setData({ ...data, password: e.target.value })}
+                placeholder="Masukkan Password"
+                required
               />
             </div>
           </div>
@@ -50,13 +56,18 @@ export function FormLogin({type, HandleSubmit, setData, data, isPending}: FormAc
         <CardFooter className="flex-col mt-8 gap-2">
           <Button disabled={isPending} type="submit" className="w-full">
             Login
-            {isPending? (<Spinner/>): ''}
+            {isPending ? <Spinner /> : ""}
           </Button>
-          <Button onClick={()=> navigate('/register')} variant="outline" className="w-full">
+          <Button
+            type="button"
+            onClick={() => navigate("/register")}
+            variant="outline"
+            className="w-full"
+          >
             Register
           </Button>
         </CardFooter>
-       </form>
+      </form>
     </Card>
-  )
+  );
 }
