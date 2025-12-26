10f69b8e230efcaa0dfc2f5d4a961ac52daa50fa
FelienZ <rajabfadh@gmail.com>
2025-12-26 16:53:48 +0700

    fit out Auth Interface & types

--- src/utils/types/Auth/Request/Auth.ts
+++ src/utils/types/Auth/Request/Auth.ts
@@ -1,11 +1,12 @@
-import type { Dispatch, SetStateAction } from "react"
 import type { User } from "../Response/User"
 import type { UserDetail } from "../Response/UserDetail"
 
 export type AuthState = {
     account: User<UserDetail> | null,
-    setAccount: Dispatch<SetStateAction<User<UserDetail>>> | null,
     isAuthenticated: boolean,
     isLoading: boolean,
-    isError: boolean
+    isError: boolean,
+    login: (data: User<UserDetail>)=>void,
+    revalidateAuth: ()=> void,
+    logout: ()=> void
 }
--- src/utils/types/Auth/Response/User.ts
+++ src/utils/types/Auth/Response/User.ts
@@ -2,6 +2,7 @@ export interface User<T>{
     username: string | null;
     id: string | null,
     email: string | null,
-    detail?: T | null
+    detail?: T | null,
+    created_at: string | null
 }
 // ini utk userData Response
